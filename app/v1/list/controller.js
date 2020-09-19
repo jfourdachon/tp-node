2bd665508971524c620547f17cd32a0985f21d44
yoannduc <dev.yoannduc@gmail.com>
2020-09-19 17:33:00 +0200

    Impelemtn detailed route

--- app/v1/list/controller.js
+++ app/v1/list/controller.js
@@ -24,4 +24,27 @@ const getAll = async (req, res) => {
   return res.status(200).json(list)
 }
 
-module.exports = { getAll }
+const getById = async (req, res) => {
+  const id = R.path(['params', 'id'])(req)
+  if (R.isNil(id)) {
+    const error = 'Id not found'
+    logger.info(error)
+    return res.status(400).json({ error })
+  }
+
+  const [listErr, list] = await to(db('list').select().where({ id }))
+  if (!R.isNil(listErr)) {
+    logger.info(`${listErr}`)
+    return res.status(500).json({ error: `${listErr}` })
+  }
+
+  if (!R.length(list)) {
+    const error = `No list for id ${id}`
+    logger.info(error)
+    return res.status(400).json({ error })
+  }
+
+  return res.status(200).json(list[0])
+}
+
+module.exports = { getAll, getById }
--- app/v1/list/router.js
+++ app/v1/list/router.js
@@ -3,5 +3,6 @@ const router = require('express').Router()
 const controller = require('./controller.js')
 
 router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
 
 module.exports = router
