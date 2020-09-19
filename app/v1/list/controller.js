ae18969c070097b086fb2fab556ff32571fe6705
yoannduc <dev.yoannduc@gmail.com>
2020-09-19 17:33:40 +0200

    Use correct log for errors

--- app/v1/list/controller.js
+++ app/v1/list/controller.js
@@ -17,7 +17,7 @@ const getAll = async (req, res) => {
       ),
   )
   if (!R.isNil(listErr)) {
-    logger.info(listErr)
+    logger.error(listErr)
     return res.status(500).json({ error: `${listErr}` })
   }
 
@@ -28,19 +28,19 @@ const getById = async (req, res) => {
   const id = R.path(['params', 'id'])(req)
   if (R.isNil(id)) {
     const error = 'Id not found'
-    logger.info(error)
+    logger.error(error)
     return res.status(400).json({ error })
   }
 
   const [listErr, list] = await to(db('list').select().where({ id }))
   if (!R.isNil(listErr)) {
-    logger.info(`${listErr}`)
+    logger.error(`${listErr}`)
     return res.status(500).json({ error: `${listErr}` })
   }
 
   if (!R.length(list)) {
     const error = `No list for id ${id}`
-    logger.info(error)
+    logger.error(error)
     return res.status(400).json({ error })
   }
 
