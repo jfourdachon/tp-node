be8e2219af18c502a406a87994b46ff93cc3ac74
yoannduc <dev.yoannduc@gmail.com>
2020-09-19 17:27:06 +0200

    Implement knex

--- app/v1/list/controller.js
+++ app/v1/list/controller.js
@@ -1,6 +1,27 @@
-const getAll = (req, res) => {
-  // TODO
-  return res.sendStatus(501)
+const { to } = require('await-to-js')
+const R = require('ramda')
+
+const db = require.main.require('./helpers/db.js')
+const logger = require.main.require('./helpers/logger.js')
+
+const getAll = async (req, res) => {
+  const title = R.path(['query', 'title'])(req)
+
+  const [listErr, list] = await to(
+    db('list')
+      .select()
+      .where((builder) =>
+        !R.isNil(title)
+          ? builder.where('title', 'like', `%${title}%`)
+          : builder,
+      ),
+  )
+  if (!R.isNil(listErr)) {
+    logger.info(listErr)
+    return res.status(500).json({ error: `${listErr}` })
+  }
+
+  return res.status(200).json(list)
 }
 
 module.exports = { getAll }
--- docker/db-init/seeder-1-list.sql
+++ docker/db-init/seeder-1-list.sql
@@ -1,1 +1,2 @@
 INSERT INTO list (title) VALUES ('My first list');
+INSERT INTO list (title) VALUES ('My second list');
